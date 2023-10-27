db9aa2f474c4f0db0c0238d49502fe0e95e9d82b
watariRyo <72895473+watariRyo@users.noreply.github.com>
2023-10-27 20:11:23 +0900

    add form api

--- controllers/controller.go
+++ controllers/controller.go
@@ -1,6 +1,11 @@
 package controllers
 
 import (
+	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"net/http"
+
 	"github.com/watariRyo/go-sample-framework/framework"
 )
 
@@ -49,7 +54,18 @@ func UsersController(ctx *framework.MyContext) {
 }
 
 func PostsController(ctx *framework.MyContext) {
-	ctx.WriteString("posts")
+	name := ctx.FormKey("name", "defaultName")
+	age := ctx.FormKey("age", "30")
+	fileInfo, err := ctx.FormFile("file")
+	if err != nil {
+		ctx.WriteHeader(http.StatusInternalServerError)
+	}
+	err = ioutil.WriteFile(fmt.Sprintf("%s %s %s", name, age, fileInfo.Filename), fileInfo.Data, fs.ModePerm)
+	if err != nil {
+		ctx.WriteHeader(http.StatusInternalServerError)
+	}
+
+	ctx.WriteString("success")
 }
 
 func PostsPageController(ctx *framework.MyContext) {
@@ -60,9 +76,15 @@ func PostsPageController(ctx *framework.MyContext) {
 		</head>
 		<body>
 			<div>
-				<form action="/posts" method="post">
-					<input name="name"/>
-					<button type="submit">submit</submit>
+				<form action="/posts" method="post" enctype="multipart/form-data">
+					<div><label>name</label>: <input name="name"/></div>
+					<div><label>age</label>: 
+					<select name="age">
+						<option value="1">1</option>
+						<option value="2">2</option>
+					</select></div>
+					<button type="submit">submit</button>
+					<input name="file" type="file"/>
 				</form>
 			</div>
 		</body>
--- framework/context.go
+++ framework/context.go
@@ -3,7 +3,9 @@ package framework
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"net/textproto"
 )
 
 type MyContext struct {
@@ -37,6 +39,10 @@ func (ctx *MyContext) WriteString(data string) {
 	fmt.Fprint(ctx.rw, data)
 }
 
+func (ctx *MyContext) WriteHeader(httpCode int) {
+	ctx.rw.WriteHeader(httpCode)
+}
+
 func (ctx *MyContext) QueryAll(query string) map[string][]string {
 	return ctx.r.URL.Query()
 }
@@ -64,4 +70,40 @@ func (ctx *MyContext) GetParam(key string, defaultValue string) string {
 	}
 
 	return defaultValue
+}
+
+func (ctx *MyContext) FormKey(key string, defaultValue string) string {
+	if ctx.r.Form == nil {
+		ctx.r.ParseMultipartForm(32 << 20)
+	}
+	if vs := ctx.r.Form[key]; len(vs) > 0 {
+		return vs[0]
+	}
+	return defaultValue
+}
+
+type FormFileInfo struct {
+	Data     []byte
+	Filename string
+	Header   textproto.MIMEHeader
+	Size     int64
+}
+
+func (ctx *MyContext) FormFile(key string) (*FormFileInfo, error) {
+	file, fileHeader, err := ctx.r.FormFile(key)
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+
+	return &FormFileInfo{
+		Data:     data,
+		Filename: fileHeader.Filename,
+		Header:   fileHeader.Header,
+		Size:     fileHeader.Size,
+	}, nil
 }
